c5e736d6b63d08a18623ce9481fcc5d166f6342d
karanaval <st.anastacie.v@gmail.com>
2023-02-14 17:20:08 +0200

    Models validation add

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,3 +1,6 @@
 class Operation < ApplicationRecord
   belongs_to :category
+
+  validates :amount, presence: true, numericality { grater_than: 0 }
+  validates :odate, :description, presence: true
 end
