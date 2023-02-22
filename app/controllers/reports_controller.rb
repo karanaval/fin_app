1fa5b46c529247fcd9f6b1ef8a606a5ccbafd118
karanaval <st.anastacie.v@gmail.com>
2023-02-22 09:57:25 +0200

    report by all categories

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,6 +5,26 @@ class ReportsController < ApplicationController
   end
 
   def report_by_category
+
+    categories_options = Category.all.map{ |cat| [cat.id, cat.name] }
+
+    operations_data = Operation.all.map { |op| [op.category_id.to_s, op.amount] }.sort_by { |element| element[0] }
+
+    operations_per_cat_id = operations_data.inject({}) do |acc, op_per_cat|
+      cat_id, amount = *op_per_cat
+      acc[cat_id] =  acc[cat_id] ? acc[cat_id] += amount : amount
+      acc
+    end
+
+    operations_per_cat_name = []
+    for i in (0...operations_per_cat_id.length) do
+      key = categories_options[i][0]
+      operations_per_cat_name.push([categories_options[i][1], operations_per_cat_id["#{key}"]])
+    end
+
+  @categories_ids = operations_per_cat_name.map { |data| data[0] }
+  @amounts_per_categories = operations_per_cat_name.map { |data| data[1].round() }
+
   end
 
   def report_by_dates
@@ -12,13 +32,13 @@ class ReportsController < ApplicationController
     operations_data = Operation.all.map { |op| [op.odate.to_s, op.amount] }.sort_by { |element| element[0] }
 
     operations_per_date = operations_data.inject({}) do |acc, op_per_date|
-        date, operations = *op_per_date
-        acc[date] =  acc[date] ? acc[date] += operations : operations
+        date, amount = *op_per_date
+        acc[date] =  acc[date] ? acc[date] += amount : amount
         acc
       end
 
     @dates = operations_per_date.map { |data| data[0] }
-    @amounts = operations_per_date.map { |data| data[1] }
+    @amounts_per_dates = operations_per_date.map { |data| data[1] }
 
   end
 end
