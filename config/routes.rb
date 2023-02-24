cf2d684ec309a6c8dfeff21e905bb508508eb749
karanaval <st.anastacie.v@gmail.com>
2023-02-24 17:26:41 +0200

    reports logik all works

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+
   get 'reports/index'
+
+  post 'reports/chose_report_type', to: 'reports#chose_report_type'
+
   get 'reports/report_by_category'
+  post 'reports/report_by_category'
+
   get 'reports/report_by_dates'
+  post 'reports/report_by_dates'
 
   root 'main#index'
   get 'main/index'
